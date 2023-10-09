226f41663369115499463d83bacee72cef57efc0
JoeBilly8 <joebilly8@hotmail.co.uk>
2023-10-09 13:42:00 +0100

    playing around + css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import './App.css';
 import Navbar from './navbar';
 import Home from './home';
 
 function App() {
-
-  const title = 'Welcome to my shitty blog';
-  const likes = 50;
-  const person = { name: 'pisspants', age: 1 };
-
   return (
     <div className="App">
       <Navbar />
--- src/home.js
+++ src/home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const clicked = () => {
+        console.log("clicked");
+    }
+
+    const clickedAgain = (name, e) => {
+        console.log("hello" + name);
+        console.log(e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={clicked}>clickage</button>
+            <button onClick={(e) => clickedAgain("shreevesh", e)}>click again u wetwipe</button>
         </div>
      );
 }
